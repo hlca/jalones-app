d1a2f38294081d5897a4baa90cf8e19a20a009b0
adriano515 <valdezadriano@gmail.com>
2016-10-04 18:29:29 -0600

    logout on dropdown

--- www/js/state1Ctrl.js
+++ www/js/state1Ctrl.js
@@ -1,10 +1,15 @@
 angular.module('starter.state1Ctrl', ['ionic'])
 
-  .controller('State1Ctrl', function($ionicPopover, $scope) {
+  .controller('State1Ctrl', function($ionicPopover, $scope,$location) {
 
     $ionicPopover.fromTemplateUrl('popoverID', {
       scope: $scope,
     }).then(function(popover) {
       $scope.popover = popover;
     });
+
+    $scope.logoutFunction = function() {
+      $location.path('/login');
+    }
+
   });
