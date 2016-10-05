618de03fde49b6e7ade417093418ea4f2fc4918e
adriano515 <valdezadriano@gmail.com>
2016-10-04 18:29:11 -0600

    tab positioning

--- www/js/app.js
+++ www/js/app.js
@@ -39,12 +39,15 @@ angular.module('starter', ['ionic',
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
+
+  $ionicConfigProvider.tabs.position('bottom');
+
   $stateProvider
   .state('login', {
     url: '/login',
