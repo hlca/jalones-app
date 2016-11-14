fb4839a5c666a8fddcbd817e3ee96934d4ce1bef
wiichog <gar14189@uvg.edu.gt>
2016-11-14 16:24:51 -0600

    RegisterCtrl

--- www/js/registerCtrl.js
+++ www/js/registerCtrl.js
@@ -1,5 +1,37 @@
-angular.module('starter.registerCtrl', [])
+angular.module('starter.RegisterCtrl', [])
 
-.controller('RegisterCtrl', function($location, $scope) {
-
+.controller('RegisterCtrl', function($location, $scope, JWTFactory) {
+  $scope.RegisterCtrl = function(){
+    var first_name = document.getElementById("first_name").value;
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var password2 = document.getElementById("password2").value;
+    var address = document.getElementById("address").value;
+    var phone = document.getElementById("phone").value;
+    JWTFactory.post(
+      'api/user/', 
+      {
+        'name': first_name, 
+        'username': first_name, 
+        'email': email, 
+        'password': password, 
+        'confirmPassword': password2, 
+        'address': address
+        'phone': phone
+      }, 
+      true, 
+      JWTFactory.serverToken.access_token
+    ).then(function(data){
+      console.log(data);
+      if(data.success) {
+        $location.path('/login');
+      }
+      else {
+        alert(data.message);
+      }
+    }, function(data){
+      alert("Hubo un error en la solicitud");
+    })
+  }
 });
